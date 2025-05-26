25bf4afe7d4a726df8336e230f65db73eef4f1ad
earlalex <contact@earlalex.com>
2025-05-26 11:28:50 -0700

    Cleaned up the hero and menu and also started to componetize the parts of the site.

---
+++ components/base-component.js
@@ -0,0 +1,14 @@
+export class BaseComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    // Override in child components
+  }
+}
---
+++ components/header.js
@@ -0,0 +1,145 @@
+import {BaseComponent} from './base-component.js';
+
+class Header extends BaseComponent {
+  constructor() {
+    super();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+        <style>
+            :host {
+            display: block;
+            }
+            .navbar {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 100%;
+            top: 0;
+            z-index: 1000;
+            background: rgba(0,0,0,0.9);
+            }
+            .brand-logo {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-decoration: none;
+            height: 175px;
+            color: white;
+            }
+            .logo-image {
+            height: 100%;
+            width: auto;
+            object-fit: contain;
+            }
+            .header-cta {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 0;
+            width: 100%;
+            }
+            .header-cta > details[open] {
+            text-align: center;
+            }
+            .header-cta > details[open] > .header-menu {
+            display: flex;
+            gap: 1rem;
+            justify-content: center;
+            align-items: center;
+            }
+            .header-menu {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1rem;
+            justify-content: center;
+            background-color: var(--deep-blue);
+            padding: 1rem 0;
+            }
+            .header-menu span {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            }
+            details {
+              width: 100%;
+            }
+            details summary {
+            font-size: 2rem;
+            cursor: pointer;
+            text-align: center;
+            }
+            a {
+            color: white;
+            text-decoration: none;
+            }
+            hr {
+            display: block;
+            width: 100%;
+            height: 1px;
+            border: 0;
+            border-top: 1px solid #ccc;
+            margin: 1em 0;
+            padding: 0;
+            }
+            @media only screen and (min-width: 600px) {
+                .navbar {
+                    flex-direction: row;
+                    gap: 1rem;
+                }
+                .header-cta {
+                    align-items: end;
+                    justify-content: end;
+                }
+                .header-cta > details[open] {
+                    text-align: center;
+                    width: 100%;
+                }
+                .header-cta > details[open] > .header-menu {
+                    flex-direction: row;
+                    gap: 1rem;
+                    text-align: center;
+                    justify-content: space-evenly;
+                    width: 100%;
+                }
+            }
+        </style>
+        <header class="navbar">
+            <section class="header-cta">
+                <details>
+                <summary>
+                Menu
+                </summary>
+                <div class="header-menu">
+                <span>
+                <a href="https://link.clover.com/urlshortener/6JfTy8" style="text-decoration: none;">
+                    Become A Member
+                </a>
+                </span>
+                <span>
+                <a href="./pages/subscriptions.html" style="text-decoration: none;">
+                    Get Your Subscription
+                </a>
+                </span>
+                <span>
+                <a href="./pages/submit.html" style="text-decoration: none;">
+                    Submit Content
+                </a>
+                </span>
+                <span>
+                <a href="https://link.clover.com/urlshortener/VcLY26" style="text-decoration: none;">
+                    Creators Club
+                </a>
+                </span>
+                </details>
+            </section>
+        </header>
+        <hr />
+    `;
+  }
+}
+
+customElements.define('musoci-header', Header);
